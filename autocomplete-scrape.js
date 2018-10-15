76a75572a5c5e026f7931ae71d8f127fa922e126
dskrenta <dskrenta@gmail.com>
2018-10-14 21:07:25 -0700

    missing 0

--- autocomplete-scrape.js
+++ autocomplete-scrape.js
@@ -27,7 +27,7 @@ function writeFilePromise(file, data) {
 }
 
 // const results = [];
-const alphas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 1, 2, 3, 4, 5, 6, 7, 8, 9, '#', '_', '.', '+', '@', '-', '(', ')'];
+const alphas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '#', '_', '.', '+', '@', '-', '(', ')'];
 const searches = [];
 for (let i = 0; i < alphas.length; i++) {
   searches.push(`${alphas[i]}`);
