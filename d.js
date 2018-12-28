03744642b28dc6809245528b1ca6e49af0ca03d2
dskrenta <dskrenta@gmail.com>
2018-12-27 22:33:55 -0500

    demo (d.js) file

---
+++ d.js
@@ -0,0 +1,57 @@
+let stuff = 'foo';
+const name = 'dave';
+
+stuff = 'something else';
+
+let num = 7;
+
+let bool = true; 
+let stuff2 = false;
+
+const person = {
+    
+};
+
+// strings
+// booleans
+// numbers
+// arrays
+// objects
+
+// let list = ['person1', 'person2', bool, 'foo'];
+
+const obj = { name: { list: { } }, foo: 234 };
+
+/*
+const person = {
+    name: {
+        first: 'David',
+        last: 'Skrenta'
+    },
+    age: 19,
+    nickname: 'Dave',
+    interests: [
+        'Swimming',
+        'Biking',
+        'Running'
+    ]
+};
+*/
+
+// console.log(person);
+// console.log(person.name.first + ' ' + person.name.last);
+
+const foo = true;
+
+// = assignment
+// == shallow equality
+// 6 == '6' true
+// 6 === '6' false
+
+if (foo === true) {
+    console.log('Hi!');
+}
+else {
+    console.log('Bye!');
+}
+
