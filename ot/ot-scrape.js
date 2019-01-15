e20a715fda2956d1af528680dfe6efc711eef7c1
dskrenta <dskrenta@gmail.com>
2019-01-14 20:16:02 -0700

    added data format comment

--- ot/ot-scrape.js
+++ ot/ot-scrape.js
@@ -14,6 +14,10 @@ const rids = JSON.parse(ridsFileContent);
 console.log(rids);
 */
 
+/*
+{"type":"ot_poll","id":"986407-1545605754.359872","request":{"covers":2,"dateTime":"2018-12-24T19:00:00","isRedesign":true},"proxies":{"http":"http://5.79.66.2:13010","https":"http://5.79.66.2:13010"},"timestamp":"20181223225554","t":1545605754,"rid":"986407","url":"https://www.opentable.com/restaurant/profile/986407/search","response":null,"created_at":"20181223225613","updated_at":"20181223225613"}
+*/
+
 const scraper = new NodePoolScraper({
   max: 1,
   min: 1,
@@ -38,6 +42,9 @@ async function grabRequest({ url, browser }) {
       const url = await res.url();
       if (url === 'https://www.opentable.com/restaurant/profile/115402/search') {
         const json = await res.json();
+        const output = {
+
+        };
         console.log(inspect(json, false, null));
       }
     }
