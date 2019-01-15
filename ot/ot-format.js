9df84f75385167f6aa09a650217e865fcc611e23
dskrenta <dskrenta@gmail.com>
2019-01-14 21:02:03 -0700

    ot_scrape work

--- ot/ot-format.js
+++ ot/ot-format.js
@@ -9,9 +9,10 @@ const writeFileAsync = promisify(fs.writeFile);
 async function main() {
   try {
     const file = await readFileAsync('ot_scan.jsonl', 'utf-8');
-    const rids = file.trim().split('\n').map(r => r.match(/"rid":"(\d+)"/)[1]);
-    console.log(rids);
-    await writeFileAsync('./rids.json', JSON.stringify(rids));
+    // const rids = file.trim().split('\n').map(r => r.match(/"rid":"(\d+)"/)[1]);
+    const urls = file.trim().split('\n').map(r => r.match(/"url":"(.*?)"/)[1]);
+    console.log(urls);
+    await writeFileAsync('./ot_urls.json', JSON.stringify(urls));
   }
   catch (error) {
     console.error(error);
--- ot/ot-scrape.js
+++ ot/ot-scrape.js
@@ -4,63 +4,112 @@ const fs = require('fs');
 const { promisify, inspect } = require('util');
 const NodePoolScraper = require('node-pool-scraper');
 
-const OT_URL = 'https://www.opentable.com/bru-burger-bar-lexington';
+const DATA_FILE_PATH = './ot_scrape.jsonl';
 
 const readFileAsync = promisify(fs.readFile);
+const appendFileAsync = promisify(fs.appendFile);
 
-/*
 const ridsFileContent = fs.readFileSync('./rids.json', 'utf-8');
+const urlsFileContent = fs.readFileSync('./ot_urls.json', 'utf-8');
 const rids = JSON.parse(ridsFileContent);
-console.log(rids);
-*/
+const urls = JSON.parse(urlsFileContent);
 
-/*
-{"type":"ot_poll","id":"986407-1545605754.359872","request":{"covers":2,"dateTime":"2018-12-24T19:00:00","isRedesign":true},"proxies":{"http":"http://5.79.66.2:13010","https":"http://5.79.66.2:13010"},"timestamp":"20181223225554","t":1545605754,"rid":"986407","url":"https://www.opentable.com/restaurant/profile/986407/search","response":null,"created_at":"20181223225613","updated_at":"20181223225613"}
-*/
+function wait(mills) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, mills)
+  });
+}
+
+function generateOutput({
+  data = null,
+  rid,
+  url
+}) {
+  return {
+    type: 'ot_poll',
+    request: {
+      covers: 2,
+      dateTime: new Date(),
+      isRedesign: true
+    },
+    rid,
+    url: `https://www.opentable.com/restaurant/profile/${rid}/search`,
+    r_url: url,
+    response: data,
+    created_at: new Date(),
+    updated_at: new Date()
+  };
+}
 
 const scraper = new NodePoolScraper({
   max: 1,
   min: 1,
   idleTimeoutMillis: 100000,
-  headless: false,
+  headless: true,
   ignoreHTTPSErrors: true
 });
 
+let count = 0;
+
 async function grabRequest({ url, browser }) {
-  const page = await browser.newPage();
-  const status = await page.goto(url, {
-    waitUntil: ['domcontentloaded', 'load']
-  });
+  const rid = rids.pop();
+  count++;
 
-  if (!status.ok) {
-    console.error(`Cannot open ${url}`);
-    throw new Error();
-  }
+  try {
+    console.log(`Current rid: ${rid}, current rid: ${count}, remaning rids: ${rids.length}, url: ${url}`);
 
-  page.on('response', async (res) => {
-    try {
-      const url = await res.url();
-      if (url === 'https://www.opentable.com/restaurant/profile/115402/search') {
-        const json = await res.json();
-        const output = {
+    const page = await browser.newPage();
+    const status = await page.goto(url, {
+      waitUntil: ['domcontentloaded', 'load']
+    });
 
-        };
-        console.log(inspect(json, false, null));
+    if (!status.ok) {
+      console.error(`Cannot open ${url}`);
+      throw new Error();
+    }
+
+    page.on('response', async (res) => {
+      try {
+        const url = await res.url();
+        if (url === `https://www.opentable.com/restaurant/profile/${rid}/search`) {
+          const json = await res.json();
+          const output = generateOutput({ url, rid, data: json });
+          console.log(output);
+          await appendFileAsync(DATA_FILE_PATH, `${JSON.stringify(output)}\n`);
+        }
       }
+      catch (error) {
+        console.error(error);
+      }
+    });
+
+    await page.click('.b3cf2c43.faaffad1.a8e8f9b4._06bf3736')
+
+    await page.waitFor(5000);
+
+    await page.close();
+  }
+  catch (error) {
+    // console.error(error);
+    const output = generateOutput({ url, rid });
+    console.log(output);
+    await appendFileAsync(DATA_FILE_PATH, `${JSON.stringify(output)}\n`);
+  }
+  finally {
+    await wait(2000);
+
+    if (rids.length > 0) {
+      scraper.addTarget({
+        url: urls.pop(),
+        func: grabRequest
+      });
     }
-    catch (error) {
-      console.error(error);
-    }
-  });
-
-  await page.click('.b3cf2c43.faaffad1.a8e8f9b4._06bf3736')
-
-  await page.waitFor(5000);
-
-  await page.close();
+  }
 }
 
 scraper.addTarget({
-  url: OT_URL,
+  url: urls.pop(),
   func: grabRequest
 });
