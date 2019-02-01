f9a1883c6c1e87a88d8321a2e37d2e0647c52ae2
dskrenta <dskrenta@gmail.com>
2019-02-01 08:13:34 -0700

    updated ticketmasterGet.js

--- ticketmaster/ticketmasterGet.js
+++ ticketmaster/ticketmasterGet.js
@@ -9,6 +9,9 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const writeFileAsync = promisify(fs.writeFile);
+const readFileAsync = promisify(fs.readFile);
+
+const CITIES_FILE = 'cities.json';
 
 const wait = (ms) => {
   return new Promise((resolve) => {
@@ -18,15 +21,45 @@ const wait = (ms) => {
   });
 }
 
-async function get({ url }) {
+async function getPage({ 
+  givenUrl = null,
+  stateCode,
+  size = 200,
+  page = 0,
+  city,
+}) {
   try {
+    const url = givenUrl ? `https://app.ticketmaster.com${givenUrl}&apikey=${process.env.ticketmasterAPIKey}` : `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=${process.env.ticketmasterAPIKey}&stateCode=${stateCode}&city=${city}&size=${size}&page=${page}`;
     const apiRes = await fetch(url);
-    const json = await apiRes.json();
-    console.log(json);
+    const json = await apiRes.text();
+    const data = JSON.parse(data);
+    page = data.page.number;
+    console.log(`Url: ${url}, Page: ${page}`);
+    await writeFileAsync(`./data/ticketmaster-${city}-${stateCode}-${page}.json`, json);
+    if ('next' in data._links) {
+      await wait(1000);
+      await getPage({ givenUrl: data._links.next.href });
+    }
   }
   catch (error) {
     console.error(error);
   } 
 }
 
-get({ url: `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=${process.env.ticketmasterAPIKey}&stateCode=CA&size=200` });
+async function main() {
+  try {
+    const citiesData = await readFileAsync(CITIES_FILE, 'utf-8');
+    const cities = JSON.parse(citiesData);
+    for (let cityData of cities) {
+      await getPage({
+        stateCode = cityData.stateCode,
+        city: cityData.city
+      });
+    }
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+
+main();
