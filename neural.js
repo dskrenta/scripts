02a7446926c2e24317a38c2112d1c2f0e8332a97
dskrenta <dskrenta@gmail.com>
2019-05-14 10:57:05 -0700

    starting on neural network

---
+++ neural.js
@@ -0,0 +1,84 @@
+'use strict';
+
+class NeuralNetwork {
+  constructor(x, y) {
+    this.input = x;
+    this.output = y.map(val => 0);
+    this.weights1 = [1, 2, 3, 4];
+    this.weights2 = [1, 2, 3, 4];
+    this.y = y;
+  }
+}
+
+function sigmoid(x) {
+  return 1 / (1 + (Math.E ** -x));
+}
+
+function sigmoidDerivative(x) {
+  return x * (1 - x);
+}
+
+function dotProduct(a, b) {
+  let total = 0;
+  for (let i = 0; i < a.length; i++) {
+    total += a[i] * b[i];
+  }
+  return total;
+}
+
+function feedforward() {
+
+}
+
+function nn(x, y) {
+  const input = x;
+  const output = y.map(val => 0);
+  const weights1 = [1, 1, 1, 1];
+  const weights2 = [1, 1, 1, 1];
+}
+
+/*
+import numpy as np
+
+def sigmoid(x):
+    return 1.0/(1+ np.exp(-x))
+
+def sigmoid_derivative(x):
+    return x * (1.0 - x)
+
+class NeuralNetwork:
+    def __init__(self, x, y):
+        self.input      = x
+        self.weights1   = np.random.rand(self.input.shape[1],4)
+        self.weights2   = np.random.rand(4,1)
+        self.y          = y
+        self.output     = np.zeros(self.y.shape)
+
+    def feedforward(self):
+        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
+        self.output = sigmoid(np.dot(self.layer1, self.weights2))
+
+    def backprop(self):
+        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
+        d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
+        d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
+
+        # update the weights with the derivative (slope) of the loss function
+        self.weights1 += d_weights1
+        self.weights2 += d_weights2
+
+
+if __name__ == "__main__":
+    X = np.array([[0,0,1],
+                  [0,1,1],
+                  [1,0,1],
+                  [1,1,1]])
+    y = np.array([[0],[1],[1],[0]])
+    nn = NeuralNetwork(X,y)
+
+    for i in range(1500):
+        nn.feedforward()
+        nn.backprop()
+
+    print(nn.output)
+*/
