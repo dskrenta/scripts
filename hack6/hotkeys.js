f0216f0c3a03a74456cb15e7e2a1d320210f5ef7
dskrenta <dskrenta@gmail.com>
2020-03-21 21:56:11 -0700

    script updates

--- hack6/hotkeys.js
+++ hack6/hotkeys.js
@@ -3,18 +3,20 @@
 const ioHook = require('iohook');
 const open = require('open');
 
-ioHook.start();
-
 const CTRL = 29;
 const F7 = 65;
 const G = 34;
 
 // Google hotkey
-ioHook.registerShortcut([CTRL, G], async () => {
+ioHook.registerShortcut([F7], async () => {
   try {
-    await open('https://google.com/');
+    console.log('hotkey');
+    await open('https://google.com/', {browser: 'google chrome'});
   }
   catch (error) {
     console.error('Google ctrl+g hook error', error);
   }
 });
+
+
+ioHook.start(true);
